404b0903f69b51206e8108bd32fbbe0f6a22e871
Vadim Sofin <Vadim_Sofin@epam.com>
2025-03-25 13:27:11 +0400

    Add example methods

--- java-upgrade-samples/src/main/java/Examples.java
+++ java-upgrade-samples/src/main/java/Examples.java
@@ -3,4 +3,24 @@ import java.util.HashMap;
 import java.util.ArrayList;
 
 public class Examples {
+
+    void example1() {
+        System.out.println(
+                "abc\n" +
+                "def\n" +
+                "hig"
+        );
+    }
+
+    void example2(Object obj) {
+        if (obj instanceof String && ((String) obj).length() > 10) {
+            // ...
+        }
+    }
+
+    void example3(ArrayList<Integer> list) {
+        list.add(0, 123);
+        list.get(0);
+    }
+
 }
